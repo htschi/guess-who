45d0c91a46eddad1b0e7631055fa6091bc706dea
htschi <125224273+htschi@users.noreply.github.com>
2023-03-29 17:51:51 +0200

    Eliminate card
    Added function toggleEliminated that toggle class 'eliminated' to the clicked card

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log('Guess Who?')
+
+// By clicking a card the this function will be triggered. the class eliminated (change opacity) will be add to the clicked card
+function toggleEliminated(card) {
+   card.classList.toggle('eliminated')
+}
