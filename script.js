e127da2898a731a600822ea37a2523e245999711
htschi <125224273+htschi@users.noreply.github.com>
2023-03-29 19:10:57 +0200

    Set the Mystery Card
    Pick a random card and set it to the mystery card (name and image)

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ console.log('Guess Who?')
 function toggleEliminated(card) {
    card.classList.toggle('eliminated')
 }
+
+// Create a Array with each Cards and the information regarding name and image
+let cardList = document.querySelectorAll('.card')
+let newCardList = []
+for(let i = 0; i < 20; i++) {
+   newCardList[i] = [{
+      name: cardList[i].querySelector('span').innerHTML,
+      image: cardList[i].querySelector('img').src
+   }] 
+}
+
+function selectRandomCard() {
+   // Create a random number
+   let randomIndex = Math.random()*19
+   let randomIndexRounded = Math.round(randomIndex)
+   
+   // Pick a random Card from the newCardList and set it to the mystery card
+   let randomCard = newCardList[randomIndexRounded]
+   document.querySelector('.mystery-card').querySelector('span').innerHTML = randomCard[0].name
+   document.querySelector('.mystery-pic').src = randomCard[0].image
+}
+selectRandomCard()
